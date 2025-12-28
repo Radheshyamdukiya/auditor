b5b87c1b9055e87a8208e31e46dc0ed54d8e1beb
RadheshyamDukiya <radheshyamdukiya701@gmail.com>
2025-12-28 20:47:52 +0530

    new change in cvs to xxl formet

--- front/src/components/download.jsx
+++ front/src/components/download.jsx
@@ -13,7 +13,7 @@ function Download() {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement('a');
       link.href = url;
-      link.setAttribute('download', 'users_list.csv');
+      link.setAttribute('download', 'UsersData.xlsx');
       document.body.appendChild(link);
       link.click();
       
